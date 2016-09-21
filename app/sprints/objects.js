3256ac4b82ceeccd3e1af5680d538ff50f72cae9
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-09-20 22:32:20 -0700

    Displays list of sprints

---
+++ app/sprints/objects.js
@@ -0,0 +1,16 @@
+// import { Sprint } from '././app.component';
+
+export default {
+  "name": "Objects",
+  "slides": "//slides.com/telegraphprep/telegraphprepweek1-21-33/embed",
+  "repo": "https://github.com/TelegraphPrep/08-2016-Objects",
+  "video": "https://vimeo.com/155754175/f02142b962"
+}
+
+
+// export default {
+//   "name": "Objects",
+//   "slides": "//slides.com/telegraphprep/telegraphprepweek1-21-33/embed",
+//   "repo": "https://github.com/TelegraphPrep/08-2016-Objects",
+//   "video": "https://vimeo.com/155754175/f02142b962"
+// };
